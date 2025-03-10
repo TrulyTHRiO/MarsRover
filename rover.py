fb7bd3f41e30136b388c2ea4b5844f028d28c8af
THRiO <k.c.jolliffe01@gmail.com>
2025-03-10 21:55:54 +0000

    added check to ensure the rover does not leave the plateau

--- rover.py
+++ rover.py
@@ -1,6 +1,6 @@
 class Plateau:
     def __init__(self, x, y):
-        self.boundingRect = [0,0,x,y]
+        self.boundingRect = [0,0,int(x),int(y)] # x1, y1, x2, y2 (not x1, y1, w, h)
 
     rovers = []
 
@@ -10,22 +10,25 @@ class Plateau:
         return rover
 
     def InitialiseRover(self, x, y, cardinal):
-        rover = Rover(x, y, cardinal)
+        rover = Rover(x, y, cardinal, self)
         self.rovers.append(rover)
         return rover
 
 
 
 class Rover:
-    def __init__(self, x, y, cardinal):
+    def __init__(self, x, y, cardinal, plateau):
         self.coordinates = [int(x),int(y)]
         self.cardinal = ["N", "E", "S", "W"].index(cardinal)
+        self.plateau = plateau
         
     def UpdateCoordinates(self):
         movement = input().split("M")
         for instruction in movement[0:-1]:
             self.Rotate(instruction)
-            self.Move()
+            if not self.Move():
+                self.Reset("The rover leaves the plateau. Please enter a new starting position and movement command.")
+                return
         self.Rotate(movement[-1])
 
     def Rotate(self, instruction):
@@ -46,6 +49,20 @@ class Rover:
                 self.coordinates[1] -= 1
             case 3:
                 self.coordinates[0] -= 1
+        return self.CheckPosition()
+
+    def CheckPosition(self):
+        if self.coordinates[0] < self.plateau.boundingRect[0] or self.coordinates[0] > self.plateau.boundingRect[2]:
+            return False
+        if self.coordinates[1] < self.plateau.boundingRect[1] or self.coordinates[1] > self.plateau.boundingRect[3]:
+            return False
+        return True
+    
+    def Reset(self, error):
+        info = input(error+"\n").split(" ")
+        self.__init__(info[0], info[1], info[2], self.plateau)
+        self.UpdateCoordinates()
+
 
 
 def InitialisePlateau():
